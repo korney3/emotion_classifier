e24de6748b0abae8c584c4d2fb71f6d124d0e6fc
korney3 <koren.iz3x@yandex.ru>
2022-07-27 19:59:05 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='data_science_module',
-    version='0.0.1',
+    version='0.0.2',
     author='korney3',
     author_email='koren.iz3x@yandex.ru',
     description='Testing installation of Package',
@@ -16,7 +16,8 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/korney3/fact_extractor_emotion_classifier/issues"
     },
     license='MIT',
-    packages=['data_science_module'],
+
+    packages=setuptools.find_packages(),
     install_requires=['jupyter', 'natasha', 'pandas', "plotly", 'dostoevsky', 'pyyaml', 'scikit-learn', 'nltk',
                       'translate', 'openpyxl', 'wiki-ru-wordnet'],
 )
